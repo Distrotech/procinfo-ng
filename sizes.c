edbaf84a0f11368ff812939d937fc98487184869
tabris <tabris@f5e064a9-0df1-db11-9cfb-00110941a173>
2007-08-24 03:57:18 +0000

    add sizes.c, to test the sizes of long and long long
    git-svn-id: http://svn.tabris.net/repos/procinfo-ng/trunk@117 f5e064a9-0df1-db11-9cfb-00110941a173

---
+++ sizes.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void) {
+	printf("sizeof(long): %i\n", sizeof(long));
+	printf("sizeof(long long): %i\n", sizeof(long long));
+
+	return 0;
+}

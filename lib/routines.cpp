22fdf767602b163bec80ec584ba864487ed46290
tabris <tabris@f5e064a9-0df1-db11-9cfb-00110941a173>
2012-07-14 06:19:13 +0000

    shrink procinfo by about 8000 bytes
    git-svn-id: http://svn.tabris.net/repos/procinfo-ng/trunk@335 f5e064a9-0df1-db11-9cfb-00110941a173

--- lib/routines.cpp
+++ lib/routines.cpp
@@ -78,12 +78,12 @@ const static vector <string> splitString(const string &delim, const string &str)
 		idx1 = str.find_first_not_of(delim, idx2);
 		idx2 = str.find_first_of(delim, idx1);
 	}
-	
+
 	return tokens;
 }
 
 const static inline string uint64toString(const uint64_t &num) {
-	char str[20+1]; // log10(2**64-1) = ~19.26
+	char str[32]; // log10(2**64-1) = ~19.26, 32 for alignment
 #if __WORDSIZE == 64
 	// uint64_t is 'long unsigned int' here
 	snprintf(str, 20, "%lu", num);
@@ -95,26 +95,26 @@ const static inline string uint64toString(const uint64_t &num) {
 }
 
 const static inline string int64toString(const int64_t &num) {
-	char str[20+1]; // log10(2**64-1) = ~19.26
+	char str[32]; // log10(2**64-1) = ~19.26, 32 for alignment
 #if __WORDSIZE == 64
 	// int64_t is 'long signed int' here
-	snprintf(str, 20, "%ld", num);
+	snprintf(str, 20+1, "%ld", num); // +1 for sign
 #else
 	// int64_t is 'long long signed int' here
-	snprintf(str, 20, "%lld", num);
+	snprintf(str, 20+1, "%lld", num); // +1 for sign
 #endif
 	return string(str);
 }
 
 const static inline string uint32toString(const uint32_t &num) {
-	char str[16]; // log10(2**32-1) = ~9.63
+	char str[16]; // log10(2**32-1) = ~9.63, 16 for alignment
 	snprintf(str, 10, "%u", num);
 	return string(str);
 }
 
 const static inline string int32toString(const int32_t &num) {
-	char str[16]; // log10(2**32-1) = ~9.63
-	snprintf(str, 10, "%d", num);
+	char str[16]; // log10(2**32-1) = ~9.63, 16 for alignment
+	snprintf(str, 10+1, "%d", num); // +1 for sign
 	return string(str);
 }
 
@@ -186,7 +186,7 @@ template <typename T> const static inline vector <T> subVec(const vector <T> &ve
 }
 
 template <typename T> const static inline T sumVec(const vector <T> &vec) {
-	T sum = 0;
+	register T sum = 0;
 	for(uint32_t i = vec.size() - 1; i > 0; i--)
 		sum += vec[i];
 	return sum;
@@ -248,7 +248,7 @@ static inline vector <string> readFile(const string &fileName) {
 	return readFile(fileName.c_str());
 }
 
-const static string double2StringPrecision(const double input, const uint32_t precision) {
+const static inline string double2StringPrecision(const double &input, const uint32_t precision) {
 	char fmtBuf[3+(10*2)+1]; bzero(fmtBuf, sizeof(fmtBuf));
 	snprintf(fmtBuf, 3+(10*2), "%%.%uf", precision);
 	char output[32]; bzero(output, sizeof(output));
@@ -256,78 +256,108 @@ const static string double2StringPrecision(const double input, const uint32_t pr
 	return string(output);
 }
 
-const static inline string toString2digits(const double input) {
+const static inline string toString2digits(const double &input) {
 	char output[32]; bzero(output, sizeof(output));
 	snprintf(output, 31, "%.2f", input);
 	return string(output);
 }
-const static string humanizeBigNums(const int64_t val, const uint32_t precision) {
+const static string humanizeBigNums(const int64_t &val, const uint32_t precision) {
 	const register int64_t absVal = llabs(val);
+	int32_t shiftVal = 0;
+	string suffix;
 	if(absVal >= (1LL << 60)) {
-		return double2StringPrecision(double(val) / (1LL << 60), precision) + "EiB";
+		shiftVal = 60;
+		suffix = "EiB";
 	}
 	else if(absVal >= (1LL << 50)) {
-		return double2StringPrecision(double(val) / (1LL << 50), precision) + "PiB";
+		shiftVal = 50;
+		suffix = "EiB";
 	}
 	else if(absVal >= (1LL << 40)) {
-		return double2StringPrecision(double(val) / (1LL << 40), precision) + "TiB";
+		shiftVal = 40;
+		suffix = "EiB";
 	}
 	else if(absVal >= (1LL << 30)) {
-		return double2StringPrecision(double(val) / (1 << 30), precision) + "GiB";
-	}
-	else if(absVal >= (1LL << 20)) {
-		return double2StringPrecision(double(val) / (1 << 20), precision) + "MiB";
-	}
-	else if(absVal >= (1LL << 10)) {
-		return double2StringPrecision(double(val) / (1 << 10), precision) + "KiB";
-	}
-	return double2StringPrecision(val, precision) + "B";
-}
-const static string humanizeBigNums(const uint64_t val, const uint32_t precision) {
-	if(val >= (1LL << 60)) {
-		return double2StringPrecision(double(val) / (1ULL << 60), precision) + "EiB";
-	}
-	else if(val >= (1LL << 50)) {
-		return double2StringPrecision(double(val) / (1ULL << 50), precision) + "PiB";
-	}
-	else if(val >= (1LL << 40)) {
-		return double2StringPrecision(double(val) / (1ULL << 40), precision) + "TiB";
-	}
-	else if(val >= (1LL << 30)) {
-		return double2StringPrecision(double(val) / (1 << 30), precision) + "GiB";
-	}
-	else if(val >= (1LL << 20)) {
-		return double2StringPrecision(double(val) / (1 << 20), precision) + "MiB";
-	}
-	else if(val >= (1LL << 10)) {
-		return double2StringPrecision(double(val) / (1 << 10), precision) + "KiB";
-	}
-	return double2StringPrecision(val, precision) + "B";
-}
-template <typename T> const static inline string humanizeBigNums(const T val, const uint32_t precision) {
-	const register T absVal = fabs(val);
-	if(absVal >= (1LL << 60)) {
-		return double2StringPrecision(double(val) / (1ULL << 60), precision) + "EiB";
-	}
-	else if(absVal >= (1LL << 50)) {
-		return double2StringPrecision(double(val) / (1ULL << 50), precision) + "PiB";
-	}
-	else if(absVal >= (1LL << 40)) {
-		return double2StringPrecision(double(val) / (1ULL << 40), precision) + "TiB";
-	}
-	else if(absVal >= (1 << 30)) {
-		return double2StringPrecision(double(val) / (1 << 30), precision) + "GiB";
+		shiftVal = 30;
+		suffix = "EiB";
 	}
 	else if(absVal >= (1 << 20)) {
-		return double2StringPrecision(double(val) / (1 << 20), precision) + "MiB";
+		shiftVal = 20;
+		suffix = "EiB";
 	}
 	else if(absVal >= (1 << 10)) {
-		return double2StringPrecision(double(val) / (1 << 10), precision) + "KiB";
+		shiftVal = 10;
+		suffix = "EiB";
+	} else {
+		suffix = "B";
 	}
-	return double2StringPrecision(val, precision) + "B";
+	return double2StringPrecision(double(val) / (1LL << shiftVal), precision) + suffix;
 }
-template <typename T> const static inline string humanizeBigNums(const T val) __attribute((always_inline));
-template <typename T> const static inline string humanizeBigNums(const T val) {
+const static string humanizeBigNums(const uint64_t &val, const uint32_t precision) {
+	int32_t shiftVal = 0;
+	string suffix;
+	if(val >= (1ULL << 60)) {
+		shiftVal = 60;
+		suffix = "EiB";
+	}
+	else if(val >= (1ULL << 50)) {
+		shiftVal = 50;
+		suffix = "PiB";
+	}
+	else if(val >= (1ULL << 40)) {
+		shiftVal = 40;
+		suffix = "TiB";
+	}
+	else if(val >= (1ULL << 30)) {
+		shiftVal = 30;
+		suffix = "GiB";
+	}
+	else if(val >= (1 << 20)) {
+		shiftVal = 20;
+		suffix = "MiB";
+	}
+	else if(val >= (1 << 10)) {
+		shiftVal = 10;
+		suffix = "KiB";
+	} else {
+		suffix = "B";
+	}
+	return double2StringPrecision(double(val) / (1ULL << shiftVal), precision) + suffix;
+}
+template <typename T> const static string humanizeBigNums(const T &val, const uint32_t precision) {
+	const register T absVal = fabs(val);
+	int32_t shiftVal = 0;
+	string suffix;
+	if(val >= (1LL << 60)) {
+		shiftVal = 60;
+		suffix = "EiB";
+	}
+	else if(val >= (1LL << 50)) {
+		shiftVal = 50;
+		suffix = "PiB";
+	}
+	else if(val >= (1LL << 40)) {
+		shiftVal = 40;
+		suffix = "TiB";
+	}
+	else if(val >= (1LL << 30)) {
+		shiftVal = 30;
+		suffix = "GiB";
+	}
+	else if(val >= (1 << 20)) {
+		shiftVal = 20;
+		suffix = "MiB";
+	}
+	else if(val >= (1 << 10)) {
+		shiftVal = 10;
+		suffix = "KiB";
+	} else {
+		suffix = "B";
+	}
+	return double2StringPrecision(double(val) / (1LL << shiftVal), precision) + suffix;
+}
+template <typename T> const static inline string humanizeBigNums(const T &val) __attribute((always_inline));
+template <typename T> const static inline string humanizeBigNums(const T &val) {
 	return humanizeBigNums(val, 2);
 }
 
